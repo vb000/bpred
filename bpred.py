9c0e75cf63592934721c1c4a1317724dee28a445
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-11-20 23:56:22 -0800

    Initial network and training code

--- bpred.py
+++ bpred.py
@@ -1,5 +1,8 @@
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 import sys
 
 class BranchTraceDataset(torch.utils.data.Dataset):
@@ -34,14 +37,47 @@ class BranchTraceDataset(torch.utils.data.Dataset):
     bpred_idx = (self.data[idx][0] ^ bhr) & self.bhr_mask
 
     # Label
-    label = self.data[idx][1]
+    label = 1 if (self.data[idx][1] == 1) else -1
 
     # Build the tensor item
-    bpred_idx_tensor = torch.tensor([((bpred_idx>>x) & 1) for x in range(self.bhr_len)])
-    label_tensor = torch.tensor(label)
+    bpred_idx_tensor = torch.Tensor([((bpred_idx>>x) & 1) for x in range(self.bhr_len)])
+    label_tensor = torch.Tensor([label])
     return bpred_idx_tensor, label_tensor
 
+class BPredFPNet(torch.nn.Module):
+  def __init__(self, bhr_len):
+    super(BPredFPNet, self).__init__()
+    self.fc1 = torch.nn.Linear(bhr_len, 1)
+    self.lossfunc = nn.MSELoss()
+  
+  def forward(self, data):
+    x = self.fc1(data)
+    x = F.sigmoid(x)
+    return x
+
+  def loss(self, prediction, label):
+    return self.lossfunc(prediction, label)
+
 if __name__ == '__main__':
-  btl = BranchTraceDataset(sys.argv[1], 8)
-  for i in range(100):
-    print(btl[i])
+  btl = BranchTraceDataset(sys.argv[1], 15)
+  model = BPredFPNet(15)
+  optimizer = optim.SGD(model.parameters(), lr=0.5)
+
+  try:
+    correct = 0
+    total = 0
+    for idx, (data, label) in enumerate(btl):
+      optimizer.zero_grad()
+      output = model(data)
+      loss = model.loss(output, label)
+      loss.backward()
+      optimizer.step()
+
+      if int(label) == -1:
+        correct += 1 if (output < 0) else 0
+      else:
+        correct += 1 if (output > 0) else 0
+
+      total += 1
+  finally:
+    print("correct = {} / {}".format(correct, total))
