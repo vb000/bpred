1c33c6e5ea80db8bd405f27c5915965c204c01fa
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-11-21 18:47:27 -0800

    Softmax activations

--- bpred.py
+++ bpred.py
@@ -37,31 +37,37 @@ class BranchTraceDataset(torch.utils.data.Dataset):
     bpred_idx = (self.data[idx][0] ^ bhr) & self.bhr_mask
 
     # Label
-    label = 1 if (self.data[idx][1] == 1) else -1
+    label = self.data[idx][1]
 
     # Build the tensor item
-    bpred_idx_tensor = torch.Tensor([((bpred_idx>>x) & 1) for x in range(self.bhr_len)])
-    label_tensor = torch.Tensor([label])
+    bpred_idx_tensor = torch.Tensor([[((bpred_idx>>x) & 1) for x in range(self.bhr_len)]])
+    label_tensor = torch.LongTensor([label])
+
     return bpred_idx_tensor, label_tensor
 
 class BPredFPNet(torch.nn.Module):
   def __init__(self, bhr_len):
     super(BPredFPNet, self).__init__()
-    self.fc1 = torch.nn.Linear(bhr_len, 1)
-    self.lossfunc = nn.MSELoss()
+    self.fc1 = torch.nn.Linear(bhr_len, 2)
+    self.lsf = torch.nn.LogSoftmax(1)
+    self.lossfunc = nn.CrossEntropyLoss(reduction='sum')
   
   def forward(self, data):
     x = self.fc1(data)
-    x = F.sigmoid(x)
+    x = F.relu(x)
+    x = self.lsf(x)
     return x
 
   def loss(self, prediction, label):
     return self.lossfunc(prediction, label)
 
 if __name__ == '__main__':
-  btl = BranchTraceDataset(sys.argv[1], 15)
-  model = BPredFPNet(15)
-  optimizer = optim.SGD(model.parameters(), lr=0.5)
+  BHR_LEN = 16
+  LR = 0.1
+
+  btl = BranchTraceDataset(sys.argv[1], BHR_LEN)
+  model = BPredFPNet(BHR_LEN)
+  optimizer = optim.SGD(model.parameters(), lr=LR)
 
   try:
     correct = 0
@@ -73,11 +79,12 @@ if __name__ == '__main__':
       loss.backward()
       optimizer.step()
 
-      if int(label) == -1:
-        correct += 1 if (output < 0) else 0
-      else:
-        correct += 1 if (output > 0) else 0
+      predicted_idx = torch.argmax(output)
 
+      if label == predicted_idx: correct += 1
       total += 1
+
+      if (idx > 100000):
+        exit(1)
   finally:
     print("correct = {} / {}".format(correct, total))
